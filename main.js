2bb4ec8a95c73cfe5b7e4dc43982a8deaad3b68d
Syed Sufiyan <90518636+syedsufiyan888@users.noreply.github.com>
2024-11-11 13:50:05 +0000

    updated

---
+++ main.js
@@ -0,0 +1,10 @@
+// document.body.style.backgroundColor='lightblue';
+// document.querySelector('h2').style.color='yellow'
+var number = prompt("hello world")
+number=Number.parseInt(number)
+
+if(number>5){
+    document.querySelector('h2').style.color="yellow"
+}else{
+    document.querySelector('h2').style.color="red";
+}
